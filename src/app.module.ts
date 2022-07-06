fe0cf471319a5862766fbbdd5fd89232b4910d36
mole828 <mrx800@163.com>
2022-07-06 12:24:12 +0800

    fix folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GachaController } from './gacha/gacha.controller';
-import { GachaService } from './gacha/gacha.service';
-import { TaskService } from './gacha/task/task.service';
+import { GachaController } from './ark/gacha.controller';
+import { GachaService } from './ark/gacha.service';
+import { TaskService } from './ark/task/task.service';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/moles'),
   ],
   controllers: [AppController, GachaController],
   providers: [AppService, GachaService, TaskService],
